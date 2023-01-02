746ca01d80a4194e8ff53b8494464e8cbb093a56
Tomasvii <tomasvillarrealfederco@gmail.com>
2023-01-02 20:54:16 -0300

    actualiza port para render

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Servidor corriendo");
 });
 
